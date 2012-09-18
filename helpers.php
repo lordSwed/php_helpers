a346ba5aa664df7c82463d4caeb55167f32d407a
Swed <swedswed@seznam.cz>
2012-09-18 12:08:44 +0200

    init2

---
+++ helpers.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Formaging string (phone number,...)
+ * @param string $string
+ * @param mumber $type
+ * @param string $delim
+ */
+function formatString($string, $type = 3222, $delim = " ") {
+	$return = '';
+	$string = str_replace($delim, '', $string);
+	$length = strlen($string);
+	$active = substr($type, 0, 1);
+	$type = substr($type, 1);
+	
+	for ($i = 1; $i <= $length; $i++) {
+		$return .= substr($string, 0, 1);
+		$string = substr($string, 1);
+		
+		if ($i == $active) {
+			$return .= $delim;
+			$active = substr($type, 0, 1) + $active;
+			$type = substr($type, 1);
+		}
+	}
+
+	return $return;
+}
+
